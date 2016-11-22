42ff4275afe952cdad0c7fcf0708dff2f4845f88
schoettker <lennartschoettker@hotmail.com>
2016-11-22 20:08:29 +0100

    add quote and author as paragraph to the output div and remove old quotes if any

--- script.js
+++ script.js
@@ -2,24 +2,38 @@ var button = document.getElementById('new-quote');
 var output = document.getElementById('output');
 
 button.addEventListener('click', function(event) {
+  var quote = getQuote(), quoteText = quote[0], quoteAuthor = quote[1];
+  createParagraphs(quote);
 });
 
 
-var author, text, arr;
-var url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous';
-function listener() {
-  arr = this.responseText.split('","');
-  // console.log(arr);
-}
-var request = new XMLHttpRequest();
-request.addEventListener('load', listener);
-request.open('GET', url, false);
-request.setRequestHeader("X-Mashape-Key", "2Zn0LF6bQwmshvTdSrYKATxlCiT8p1LwuSujsnzHVXLdMYdoJ0");
-request.send();
+function getQuote() {
+  var quotes = [], arr;
+  var url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous';
+  function listener() {
+    arr = this.responseText.split('","');
+    // console.log(arr);
+  }
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', listener);
+  request.open('GET', url, false);
+  request.setRequestHeader("X-Mashape-Key", "2Zn0LF6bQwmshvTdSrYKATxlCiT8p1LwuSujsnzHVXLdMYdoJ0");
+  request.send();
 
-var quotes = [];
-for (var i = 0, l = arr.length - 1; i < l; i++) {
-  var v = arr[i];
-  quotes.push(v.replace(/"*\{*"*\w+":"/, ""));
+  for (var i = 0, l = arr.length - 1; i < l; i++) {
+    var v = arr[i];
+    quotes.push(v.replace(/"*\{*"*\w+":"/, ""));
+  }
+
+  return quotes;
 }
-console.log(quotes);
+
+function createParagraphs(array) {
+  output.innerHTML = null;
+  for (var i = 0, l = array.length; i < l; i++) {
+    var v = array[i];
+    var para = document.createElement('p');
+    para.appendChild(document.createTextNode(v)); 
+    output.appendChild(para);
+  }
+}
